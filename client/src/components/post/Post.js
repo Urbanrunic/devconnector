7a45695df82344e064fab59f4f0f0d09ce7ca688
Tucker Jones <urbanrunic@gmail.com>
2018-07-31 11:57:53 -0400

    Adding Comment Form and Actions

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import PostItem from "../posts/PostItem";
+import CommentForm from "./CommentForm";
 import Spinner from "../common/Spinner";
 import { getAPost } from "../../actions/postActions";
 import { Link } from "react-router-dom";
@@ -21,6 +22,7 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </div>
       );
     }
